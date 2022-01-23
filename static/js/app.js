13b00f21d086a3ad8fcf113cf9fc4f9096dca03b
Manackal <mereenaf@bcm.edu>
2022-01-23 13:46:09 -0600

    final table

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ var tbody = d3.select("tbody");
 // Clear existing data
 function buildTable(data) {
    tbody.html(""); 
-}
+};
 
 // Loop through each object in teh data
 // and append a row and cells for each value in the row
@@ -24,3 +24,27 @@ data.forEach((dataRow) => {
  
 });
 }
+
+//Adding a function for a button-get datetime value from the filter
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the
+    // data using that date
+  if (date) {
+    // Apply 'filter' to the table data to only keep the 
+    // rows where the 'datetime' value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+  // Rebuild the table usingthe filtered data
+  // @Note: if no date was entered, then filteredData will
+  // just be the original tableData
+  buildTable(filteredData);
+};
+// Adding code to "listen" for click
+// Attach an even to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Calling the code to build table when page loads
+buildTable(tableData);
